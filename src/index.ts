4c137c85ca650f99f84027188e59fc87521457cb
olzzon <olzzon@Kaspers-MacBook-Pro.local>
2020-05-12 11:42:07 +0200

    feat: initial REST support

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,9 @@
 const httpProxy = require('http-proxy')
 const http = require('http')
 
-const proxy = new httpProxy.createProxyServer({
-    target: {
-        host: 'localhost',
-        port: 1176,
-    },
-})
-const proxyServer = new http.createServer((req: any, res: any) => {
-    proxy.web(req, res)
-})
+import { expressInit } from './expressHandler'
+import { initProxy } from './proxyHandler'
 
-proxyServer.on('upgrade', function (req: any, socket: any, head: any) {
-    proxy.ws(req, socket, head)
-})
-
-proxyServer.listen(8015)
-
-console.log('Proxy listening to 8015, and sending out to 1176')
+expressInit()
+initProxy()
 
